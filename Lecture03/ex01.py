cf9f750afe76f9774be581908781b3e063550843
Woody Lo <ability1013@gmail.com>
2018-08-08 17:25:46 +0800

    ex01

---
+++ Lecture03/ex01.py
@@ -0,0 +1,38 @@
+'''
+Exercise01 - Mathematical Functions.
+
+Chung Yuan Christian University
+written by Woody Lo (the teaching assistant of Python Course_Summer Camp.)
+'''
+
+"""
+' + ' is addition (加法).
+' - ' is subtraction (減法).
+' * ' is multiplication (乘法).
+' / ' is division (除法).
+' // ' is integer division (整數除法).
+' ** ' is exponent (指數).
+' % ' is remainder (餘數).
+"""
+
+# Addition
+print("1+5=", 1+5)   # Answar is: 6
+
+# Subtraction
+print("6-4=", 6-4)   # Answar is: 2
+
+# Multiplication
+print("3*5=", 3*5)   # Answar is: 15
+
+# Division
+print("1/5=", 1/5)   # Answar is: 0.2
+
+# Integer Division
+print("7//3=", 7//3) # Answar is: 2
+
+# Exponent
+print("2**5=", 2**5) # Answar is: 32
+
+# Remainder
+print("7%3=", 7%3)   # Answar is: 1
+
